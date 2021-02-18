885ae2d9c2e93c86a5078ea46bcfb6bfe6e36e51
Carambula <antibog6@gmail.com>
2021-02-18 19:42:21 +0200

    Merge branch 'Carambula' into develop
    # Conflicts:
    #	Assets/Prefabs/Player.prefab

---
+++ Assets/Classes/Utils.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Classes {
+    public static class Vector2Extension {
+        public static Vector2 RotateDegrees(this Vector2 vec2, float angle) {
+            angle = angle.ToRadians();
+            return RotateRadians(vec2, angle);
+        }
+
+        public static Vector2 RotateRadians(this Vector2 vec2, float angle) {
+            return new Vector2 {
+                x = vec2.x * Mathf.Cos(angle) + vec2.y * Mathf.Sin(angle),
+                y = vec2.y * Mathf.Cos(angle) - vec2.x * Mathf.Sin(angle)
+            };
+        }
+    }
+
+    public static class AngleExtension {
+        public static float ToRadians(this decimal number) {
+            return (float) number * Mathf.PI / 180;
+        }
+
+        public static float ToRadians(this float number) {
+            return number * Mathf.PI / 180;
+        }
+
+        public static float ToRadians(this int number) {
+            return number * Mathf.PI / 180;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,9 +1,7 @@
 ﻿using UnityEngine;
 
-namespace Player
-{
-    public class PlayerMovementController : MonoBehaviour
-    {
+namespace Player {
+    public class PlayerMovementController : MonoBehaviour {
         private static readonly Vector3 GUN_ROTATION_OFFSET = 90 * Vector3.up;
         private Rigidbody rigidbodyComponent;
         private PlayerAnimationController animationController;
@@ -14,39 +12,36 @@ namespace Player
 
         public float speed;
 
-        void Awake()
-        {
+        void Awake() {
             rigidbodyComponent = GetComponent<Rigidbody>();
             animationController = GetComponent<PlayerAnimationController>();
             mainCamera = mainCamera ? mainCamera : Camera.main;
         }
-        void FixedUpdate()
-        {
+
+        void FixedUpdate() {
             MovePlayer(out var moveVector);
             TurnPlayer();
             animationController.Animate(moveVector.x, moveVector.z, false);
         }
 
-        private void MovePlayer(out Vector3 moveVector)
-        {
+        private void MovePlayer(out Vector3 moveVector) {
             moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            var desiredDirection = (mainCamera.transform.forward * moveVector.z + mainCamera.transform.right * moveVector.x).normalized;
+            var desiredDirection =
+                (mainCamera.transform.forward * moveVector.z + mainCamera.transform.right * moveVector.x).normalized;
             desiredDirection = new Vector3(desiredDirection.x, transform.position.y, desiredDirection.z);
             var moveDirection = transform.position + desiredDirection * (Time.deltaTime * speed);
             rigidbodyComponent.MovePosition(moveDirection);
         }
-        private void TurnPlayer()
-        {
+
+        private void TurnPlayer() {
             var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out var hit, whatIsGround))
-            {
+            if (Physics.Raycast(ray, out var hit, whatIsGround)) {
                 var playerToMouse = hit.point - transform.position;
                 playerToMouse.y = 0f;
                 playerToMouse.Normalize();
                 rigidbodyComponent.MoveRotation(Quaternion.LookRotation(playerToMouse));
-                if (isGunRotate)
-                {
+                if (isGunRotate) { // todo Clamp rotation of gun 
                     gun.LookAt(hit.point);
                     gun.localEulerAngles += GUN_ROTATION_OFFSET;
                 }
