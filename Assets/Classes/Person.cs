13c1a34bf09afeb07ecd8be48229e441810950c4
Carambula <antibog6@gmail.com>
2021-03-04 13:25:46 +0200

    Update Person.cs

--- Assets/Classes/Person.cs
+++ Assets/Classes/Person.cs
@@ -1,12 +1,42 @@
-﻿namespace Classes
-{
-    public abstract class Person
-    {
-        protected float health;
+﻿using UnityEngine;
 
-        protected void TakeDamage(float damage)
-        {
-            health -= damage;
+namespace Classes {
+    public abstract class Person {
+        private float health;
+        public bool IsDead => Health.Equals(0);
+
+        public delegate void OnHealthZero();
+
+        public event OnHealthZero healthZero;
+
+        protected Person(float health, float maxHealth, float moveSpeed) {
+            this.health = health;
+            MaxHealth = maxHealth;
+            MoveSpeed = moveSpeed;
+            healthZero += Die;
+        }
+
+        public float Health {
+            get => health;
+            private set {
+                health = Mathf.Clamp(value, 0, MaxHealth);
+                if (health == 0)
+                    healthZero?.Invoke();
+            }
+        }
+
+        public float MaxHealth { get; }
+
+        public float MoveSpeed { get; }
+
+        public virtual void Die() { }
+
+        public void TakeDamage(float damage) {
+            Health -= damage;
+        }
+
+        public void RestoreHealth(float heal) {
+            Health += heal;
         }
     }
 }
