0939a9d396261267e8f88adfda80eef81ebae11d
9POIII <ryklik95@github.com>
2021-03-24 00:13:47 +0200

    1. add scrap object 2. update mainMenu

---
+++ Assets/Scripts/ScrapController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrapController : MonoBehaviour
+{
+    [SerializeField] private SphereCollider scrapTrigger;
+    [SerializeField] private GameObject scrapModel;
+    [SerializeField] private GameObject player;
+    [SerializeField] private ScrapInventoryController ScrapInventoryController;
+
+
+    private void OnTriggerEnter(Collider player)
+    {
+        Destroy(scrapModel);
+        ScrapInventoryController.scrapCounter += 1;
+    }
+
+}
---
+++ Assets/Scripts/ScrapInventoryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrapInventoryController : MonoBehaviour
+{
+    public int scrapCounter;
+
+    public void Start()
+    {
+        scrapCounter = 0;
+    }
+}
