94d40c0232d82a55f59239b6b9bfbfe1eab0688e
9POIII <ryklik95@github.com>
2021-04-15 22:35:52 +0300

    minimap update

---
+++ Assets/Scripts/MinimapController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    [SerializeField] GameObject human;
+    [SerializeField] GameObject robot;
+    [SerializeField] GameObject camera;
+
+    void Update()
+    {
+        if (human.activeInHierarchy == true)
+        {
+            camera.transform.position = human.transform.position + new Vector3(0, 30, 0);
+        }
+        else
+        {
+            camera.transform.position = robot.transform.position + new Vector3(0, 30, 0);
+        }
+    }
+}
