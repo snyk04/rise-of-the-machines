5fee8adc6ce6a7e429636d216188840b90fe4140
Carambula <antibog6@gmail.com>
2021-05-22 03:01:47 +0300

    fix ArmorCharacteristic armorCoefficient

--- Assets/Classes/TryInHierarchie/ArmorCharacteristic.cs
+++ Assets/Classes/TryInHierarchie/ArmorCharacteristic.cs
@@ -2,7 +2,10 @@
     public class ArmorCharacteristic : Characteristic {
 
         private float armorCoefficient;
-        public ArmorCharacteristic(float armor, float armorCoefficient = 0.05f) : base(armor) { }
+
+        public ArmorCharacteristic(float armor, float armorCoefficient = 0.05f) : base(armor) {
+            this.armorCoefficient = armorCoefficient;
+        }
 
         public float GetResistance() {
             return 1 / (1 + armorCoefficient * Value);
